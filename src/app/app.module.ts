0e3b7aaf439ce284b37f9847d1d04b70498bf2ea
Dev <dmb4086@rit.edu>
2021-02-20 20:40:35 -0500

    created component for listing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppListingComponent } from './app-listing/app-listing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppListingComponent
   ],
   imports: [
     BrowserModule,
