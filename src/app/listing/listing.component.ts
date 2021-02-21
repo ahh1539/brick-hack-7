20d31020f3e1e48e7e5af43007dec38c9060f1a7
Dev <dmb4086@rit.edu>
2021-02-21 01:17:20 -0500

    card done

--- src/app/listing/listing.component.ts
+++ src/app/listing/listing.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListingComponent implements OnInit {
 
+  // tslint:disable-next-line:ban-types
+  name: string = '';
+  cost: number;
+  condition: string = '';
+  link: string;
+
+
+
   constructor() { }
 
   ngOnInit(): void {
+
+    this.condition = 'new';
+    this.cost = 2000;
+    this.name = 'balenciaga X Predator';
+    this.link = 'https://image.goat.com/crop/375/attachments/product_template_pictures/images/049/583/687/original/544351_W2GA1_4452.png.png';
   }
 
 }
